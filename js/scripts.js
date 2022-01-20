7d98a05ffe061b39cb579d41fef13a1d72d805b4
FennMacon <fennmacon@gmail.com>
2022-01-19 19:11:59 -0500

    name js fun

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var mb = document.getElementById("imgmb").style;
 var tv = document.getElementById("imgtv").style;
 var sp = document.getElementById("imgsp").style;
 var ow = document.getElementById("imgow").style;
+var head = document.getElementById("topname");
 
 function togglemb() {
     if (mb.display == "none" || mb.display == "") {
@@ -34,3 +35,11 @@ function toggleow() {
         ow.display = "none";
     }
 }
+
+function namechange() {
+    const fonts = ['Roboto', 'Roboto Mono', 'Arial', 'Verdana', 'Helvetica', 'Tahoma', 'Trebuchet MS', 'Times New Roman', 'Georgia', 'Garamond', 'Courier New', 'Brush Script MT', 'Comic Sans'];
+    const random = Math.floor(Math.random() * fonts.length);
+
+        head.style.fontFamily = fonts[random];
+}
+
